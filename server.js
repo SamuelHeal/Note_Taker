4043bd535c8a9c1a508bb54cba1e8ac4a7b2257b
Samuel Heal <samuelheal@hotmail.com>
2021-05-18 18:37:28 +1000

    server.js updated

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
-const express = require('express');
+const notesArray = require('./db/db');
 const path = require('path');
 const fs = require('fs');
-const uniqid = require('uniqid');
-const jsonArr = require('./db/db');
-
-
+const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-let noteArr = [];
-if(jsonArr) {
-    jsonArr.forEach( nNote => {
-        noteArr.push(nNote);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+
+let storedNotes = [];
+if(notesArray.length < 0) {
+    notesArray.forEach(function(note) {
+        storedNotes.push(note);
     })
 }
 
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static('public'));
-
 app.post('/api/notes', (req, res) => {
-    const saveNote = req.body;
-    noteArr.push(saveNote);
-    // Gives a unique ID to each note so that you can call on them later
-    noteArr.forEach((note) => {
-        note.id = uniqid();
+    const noteText = req.body;
+    storedNotes.push(noteText);
+    var counter = 1
+    storedNotes.forEach((note) => {
+        var counterString = counter.toString()
+        note.id = counterString;
+        counter++
     })
-    res.json(saveNote);
-    fs.writeFile('db/db.json', JSON.stringify(noteArr, null, 2), (err) => {
+    res.json(noteText);
+    fs.writeFile('db/db.json', JSON.stringify(storedNotes), (err) => {
         if(err) {
             console.log(err)
         }
@@ -40,7 +43,7 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-    if(jsonArr) {
+    if(notesArray) {
         fs.readFile('db/db.json', (err, data) => {
             if (err) throw err;
             res.send(data);
@@ -50,27 +53,18 @@ app.get('/api/notes', (req, res) => {
 
 app.delete('/api/notes/:id', (req, res) => {
     const { id } = req.params;
-    noteArr.forEach(note => {
+    storedNotes.forEach(note => {
         if (note.id === id) {
-            let changeArr = noteArr.filter(notes => {
+            let changeArr = storedNotes.filter(notes => {
                 return notes.id !== id;
             })
-            noteArr = changeArr;
-            fs.writeFile('db/db.json', JSON.stringify(noteArr, null, 2), (err) => {
+            storedNotes = changeArr;
+            fs.writeFile('db/db.json', JSON.stringify(storedNotes), (err) => {
                 if(err) {
                     console.log(err)
                 }
             })
-            // console.log(changeArr)
             res.send(id);
         }
     })
 })
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
