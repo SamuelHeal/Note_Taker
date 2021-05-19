2870f4c9b1b56d675dc4416e2ef75eeb57e93d73
Samuel Heal <samuelheal@hotmail.com>
2021-05-19 13:49:47 +1000

    added notes in the files

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,20 @@
 const path = require('path');
 const fs = require('fs');
 
+// Exporting all the below code
 module.exports = (app) => {
     
+    // Getting the index.html file
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'))
     });
     
+    // Getting the notes.html file
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
     
+    // Getting the db.json file
     app.get("/api/notes", (req, res) => {
         let notes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
         res.json(notes);
@@ -18,6 +22,7 @@ module.exports = (app) => {
     
     let storedNotes = [];
 
+    // Function to save notes
     app.post('/api/notes', (req, res) => {
         let noteText = req.body;
         storedNotes.push(noteText);
@@ -33,6 +38,7 @@ module.exports = (app) => {
         })
     });
     
+    // Function to delete notes
     app.delete('/api/notes/:id', (req, res) => {
         let {id} = req.params
         let currentNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+// Making a connection to local port
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
+// Acquiring the code from routes.js file
 require("./routes/routes")(app)
 
 
